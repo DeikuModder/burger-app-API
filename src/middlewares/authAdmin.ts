e4151b7d3e9777aa5c6bbd5799920f46a152b8f1
DeikuModder <gabrieltc555@gmail.com>
2023-12-11 01:51:29 -0400

    fix admin credentials

--- src/middlewares/authAdmin.ts
+++ src/middlewares/authAdmin.ts
@@ -1,10 +1,12 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { UserForToken } from "../types";
+import mongoose from "mongoose";
 
 async function authAdmin(req: Request, res: Response, next: NextFunction) {
   try {
     const authorization = req.get("authorization");
+    const adminId = process.env.ADMINID as unknown as mongoose.Types.ObjectId;
     let token: string = "";
     let decodedToken: UserForToken;
 
@@ -17,7 +19,7 @@ async function authAdmin(req: Request, res: Response, next: NextFunction) {
       process.env.SECRET as jwt.Secret
     ) as UserForToken;
 
-    if (decodedToken.username !== "Gab") {
+    if (decodedToken.username !== "Gab" && decodedToken.id !== adminId) {
       return res.status(405).send("You don't have admin permissions");
     }
 
